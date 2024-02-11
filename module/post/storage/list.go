f99081657ffa213ae77189598a93e0b0ad9b9ae4
unknown <leductoan3082004@gmail.com>
2024-02-11 23:38:30 +0700

    chore: return empty array

--- module/post/storage/list.go
+++ module/post/storage/list.go
@@ -49,5 +49,9 @@ func (s *mongodbStore) ListDataWithCondition(ctx context.Context, condition bson
 	if err = cursor.All(ctx, &res); err != nil {
 		return nil, appCommon.ErrDB(err)
 	}
+
+	if res == nil {
+		return []postmodel.SimplePost{}, nil
+	}
 	return res, nil
 }
