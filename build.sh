3e70704fe62a55974b5dc59a9e53006010bd7fa6
unknown <leductoan3082004@gmail.com>
2024-02-28 10:32:25 +0700

    add test

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 APP_NAME=nckh-user-service
 
-docker rmi ${APP_NAME}:latest
+./deploy/remove.sh
+
 docker build -t ${APP_NAME}:latest .
 docker tag ${APP_NAME}:latest toan3082004/${APP_NAME}:latest
 docker push toan3082004/${APP_NAME}:latest
--- deploy/remove.sh
+++ deploy/remove.sh
@@ -1,3 +1,5 @@
-docker rmi -f toan3082004/nckh:latest
-docker rmi -f nckh:latest
-docker untag toan3082004/nckh:latest
+APP_NAME=nckh-user-service
+
+docker rmi -f toan3082004/nckh-user-service:latest
+docker rmi -f nckh-user-service:latest
+docker rmi $(docker images -f "dangling=true" -q)
